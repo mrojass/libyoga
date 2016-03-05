8ee7920a6a6b2eea3cb0fd481f02f3cdc0688e34
Trevor Helms <thelms501@gmail.com>
2016-03-05 18:06:11 -0500

    Images and geomeometry

--- src/geometry/edge.py
+++ src/geometry/edge.py
@@ -4,29 +4,56 @@ from scipy import ndimage as ndi
 from skimage import feature
 from skimage import io
 
-img = io.imread("../../img/dd-male2.jpg", flatten=True)
-edges1 = feature.canny(img)
-edges2 = feature.canny(img, sigma=4.5)
-out = np.uint8(edges2 * 255)
+import sys, getopt
 
-io.imsave('../../img/gs-edge-male2.jpg', out)
+VALUE_SIGMA = 4.5
 
-# display results
-fig, (ax1, ax2, ax3) = plt.subplots(nrows=1, ncols=3, figsize=(8, 3), sharex=True, sharey=True)
+def main(argv):
+    ifile = ''
+    ofile = ''
+    try:
+        opts, args = getopt.getopt(argv, 'hi:o:', ['ifile=', 'ofile='])
+    except getopt.GetoptError:
+        print 'test.py -i <inputfile> -o <outputfile>'
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print 'test.py -i <inputfile> -o <outputfile>'
+            sys.exit()
+        elif opt in ("-i", "--ifile"):
+            ifile = arg
+        elif opt in ("-o", "--ofile"):
+            ofile = arg
 
-ax1.imshow(img, cmap=plt.cm.jet)
-ax1.axis('off')
-ax1.set_title('noisy image', fontsize=20)
+    edge(ifile, ofile)
 
-ax2.imshow(edges1, cmap=plt.cm.gray)
-ax2.axis('off')
-ax2.set_title('Canny filter, $\sigma=1$', fontsize=20)
+def edge(ifile, ofile):
+    img = io.imread(ifile, flatten=True)
+    edges1 = feature.canny(img)
+    edges2 = feature.canny(img, sigma=VALUE_SIGMA)
+    out = np.uint8(edges2 * 255)
 
-ax3.imshow(edges2, cmap=plt.cm.gray)
-ax3.axis('off')
-ax3.set_title('Canny filter, $\sigma=4.5$', fontsize=20)
+    io.imsave(ofile, out)
 
-fig.subplots_adjust(wspace=0.02, hspace=0.02, top=0.9,
-                    bottom=0.02, left=0.02, right=0.98)
+    # display results
+    fig, (ax1, ax2, ax3) = plt.subplots(nrows=1, ncols=3, figsize=(8, 3), sharex=True, sharey=True)
 
-plt.show()
+    ax1.imshow(img, cmap=plt.cm.jet)
+    ax1.axis('off')
+    ax1.set_title('noisy image', fontsize=20)
+
+    ax2.imshow(edges1, cmap=plt.cm.gray)
+    ax2.axis('off')
+    ax2.set_title('Canny filter, $\sigma=1$', fontsize=20)
+
+    ax3.imshow(edges2, cmap=plt.cm.gray)
+    ax3.axis('off')
+    ax3.set_title('Canny filter, $\sigma=' + str(VALUE_SIGMA) + '$', fontsize=20)
+
+    fig.subplots_adjust(wspace=0.02, hspace=0.02, top=0.9,
+                        bottom=0.02, left=0.02, right=0.98)
+
+    plt.show()
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
--- src/geometry/hough.py
+++ src/geometry/hough.py
@@ -1,60 +1,56 @@
+"""
+Module for applying straight line Hough transformations to processed images (
+specifically ones that have been processed with a Canny edge detection
+algorithm) of yoga poses.
+"""
 from math import degrees
-from matplotlib import cm
-from skimage.transform import (hough_line, hough_line_peaks,
-                               probabilistic_hough_line)
-from skimage.feature import canny
-from skimage import data
-from skimage.io import imread
 import numpy as np
+from skimage.transform import (hough_line, hough_line_peaks)
+from skimage.io import imread
 import matplotlib.pyplot as plt
 
-#
-image1 = imread("../../img/dd-male2.jpg", flatten=True)
-image2 = imread("../../img/gs-edge-blur-male2.jpg", flatten=True)
-image3 = imread("../../img/gs-edge-male2.jpg", flatten=True)
 
-h, theta, d = hough_line(image1)
+def lines(base, test):
+    """
+    Reads two images of yoga poses and compares them.
+    """
+    image1 = imread("../../img/partner-A/gs-edge-females.jpg", flatten=True)
+    image2 = imread("../../img/gs-edge-jarrod-deanna.jpg", flatten=True)
 
-fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(8,4))
+    # Generate figure and 2 axes from matplotlib
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(8, 4))
 
-print "Plot 1"
-ax1.imshow(image1, cmap=plt.cm.gray)
-rows, cols = image1.shape
-for _, angle, dist in zip(*hough_line_peaks(h, theta, d)):
-    y0 = (dist - 0 * np.cos(angle)) / np.sin(angle)
-    y1 = (dist - cols * np.cos(angle)) / np.sin(angle)
-    print degrees(angle)
-    ax1.plot((0, cols), (y0, y1), '-r')
-ax1.axis((0, cols, rows, 0))
-ax1.set_title('Detected lines')
-ax1.set_axis_off()
+    # Plot 1 -> Base Case (original)
+    print "Plot 1"
+    h, theta, d = hough_line(image1)
+    ax1.imshow(image1, cmap=plt.cm.get_cmap('gray'))
+    rows, cols = image1.shape
 
-h1, theta1, d1 = hough_line(image2)
+    for _, angle, dist in zip(*hough_line_peaks(h, theta, d)):
+        y0 = (dist - 0 * np.cos(angle)) / np.sin(angle)
+        y1 = (dist - cols * np.cos(angle)) / np.sin(angle)
+        print degrees(angle)
+        ax1.plot((0, cols), (y0, y1), '-r')
 
-print "\nPlot 2"
-ax2.imshow(image2, cmap=plt.cm.gray)
-rows, cols = image2.shape
-for _, angle, dist in zip(*hough_line_peaks(h1, theta1, d1)):
-    y0 = (dist - 0 * np.cos(angle)) / np.sin(angle)
-    y1 = (dist - cols * np.cos(angle)) / np.sin(angle)
-    print degrees(angle)
-    ax2.plot((0, cols), (y0, y1), '-r')
-ax2.axis((0, cols, rows, 0))
-ax2.set_title('Detected lines')
-ax2.set_axis_off()
+    ax1.axis((0, cols, rows, 0))
+    ax1.set_title('Detected lines')
+    ax1.set_axis_off()
 
-h2, theta2, d2 = hough_line(image3)
 
-print "\nPlot 3"
-ax3.imshow(image3, cmap=plt.cm.gray)
-rows, cols = image3.shape
-for _, angle, dist in zip(*hough_line_peaks(h2, theta2, d2)):
-    y0 = (dist - 0 * np.cos(angle)) / np.sin(angle)
-    y1 = (dist - cols * np.cos(angle)) / np.sin(angle)
-    print degrees(angle)
-    ax3.plot((0, cols), (y0, y1), '-r')
-ax3.axis((0, cols, rows, 0))
-ax3.set_title('Detected lines')
-ax3.set_axis_off()
+    # Plot 2 -> Test Case (submission)
+    print "\nPlot 2"
+    h1, theta1, d1 = hough_line(image2)
+    ax2.imshow(image2, cmap=plt.cm.gray)
+    rows, cols = image2.shape
 
-plt.show()
+    for _, angle, dist in zip(*hough_line_peaks(h1, theta1, d1)):
+        y0 = (dist - 0 * np.cos(angle)) / np.sin(angle)
+        y1 = (dist - cols * np.cos(angle)) / np.sin(angle)
+        print degrees(angle)
+        ax2.plot((0, cols), (y0, y1), '-r')
+
+    ax2.axis((0, cols, rows, 0))
+    ax2.set_title('Detected lines')
+    ax2.set_axis_off()
+
+    plt.show()
